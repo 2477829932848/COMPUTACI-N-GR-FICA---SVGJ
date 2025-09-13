d749b6f6882a27135b6de1e3518c411e1346343a
Jhonnfraf <147097991+Jhonnfraf@users.noreply.github.com>
2025-09-13 18:14:07 -0500

    "complemento Taller 4 Sara Guevara"

--- proyecto_angular_saraguevara/src/app/app.component.ts
+++ proyecto_angular_saraguevara/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MiComponenteComponent } from './mi-componente/mi-componente.component';
 import { PrimeraparteComponent } from './primera-parte/tabla.component';
+import { Punto2Component } from './segunda-parte/punto2.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MiComponenteComponent, PrimeraparteComponent],
+  imports: [RouterOutlet, MiComponenteComponent, PrimeraparteComponent, Punto2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
